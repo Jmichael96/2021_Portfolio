cb10325717483c85548f6ae77f4e213b9bcefa91
Jeffrey VanHorn <jeffreyvh@ymail.com>
2021-01-07 12:10:13 -0600

    changed header to a better set of graphics and design

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,10 +10,36 @@ document.onreadystatechange = function () {
       document.querySelector('#loader').style.display = 'none';
       document.querySelector('nav').style.display = 'flex';
       document.querySelector('body').style.visibility = 'visible';
+
+      // ! RENDERING THE NAME
+      let nameString = 'jeffrey vanhorn';
+      nameString.split('').map(function (char, index) {
+        // check if there is a white space char in the string
+        if (char.indexOf(' ') >= 0) {
+          $('#headerName').append(`<span style="margin: 0 1.5rem;">${char}</span>`);
+          return;
+        }
+        $('#headerName').append(`<span class="nameChar" style="animation-delay: ${.5 + index / 4}s;">${char}</span>`);
+
+      });
+
+      // ! RENDERING THE HEADER TEXT
+      setTimeout(() => {
+        renderHeaderText();
+      }, 4500);
       // document.getElementById('introNameVid').style.display = 'block';
-      document.getElementById('videoName').innerHTML = `<video id="introNameVid" defaultMuted autoplay="autoplay" muted><source src="./assets/images/bg/officialName.mp4" type="video/mp4">Your browser does not support the video tag.</video>`
+      // document.getElementById('videoName').innerHTML = `<video id="introNameVid" defaultMuted autoplay="autoplay" muted><source src="./assets/images/bg/officialName.mp4" type="video/mp4">Your browser does not support the video tag.</video>`
     }, 2000);
   }
+};
+
+// render the header text html to the dom
+function renderHeaderText() {
+  document.getElementById('headerTextRender').innerHTML = `<div id="headerTextWrap">
+  <div class="headerTextDash"></div>
+  <p id="headerText">full-stack developer</p>
+  <div class="headerTextDash"></div>
+  </div>`;
 };
 
 // smooth scroll function
