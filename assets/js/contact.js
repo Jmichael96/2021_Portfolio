6c33f747a4c1bbe103c313f6ea51b1c10f7d19f3
Jeffrey VanHorn <jeffreyvh@ymail.com>
2021-01-15 10:53:50 -0600

    added custom map

---
+++ assets/js/contact.js
@@ -0,0 +1,9 @@
+// idwJh8J0x9cq7vGdT0SgaIkuv0Gdb1pM
+var map = tt.map({
+    key: 'idwJh8J0x9cq7vGdT0SgaIkuv0Gdb1pM',
+    container: 'map',
+    basePath: '/',
+    style: '/custommap.json',
+    source: 'vector'
+});
+//tomtom://vector/1/basic-main
