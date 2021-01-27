7d6e963ddb06c77befb7f00a519471685024a045
Jeffrey VanHorn <jeffreyvh@ymail.com>
2021-01-26 18:49:18 -0600

    added neptune

--- assets/js/planets.js
+++ assets/js/planets.js
@@ -1,34 +1,50 @@
-var canvas, canvas2, stage, exportRoot, anim_container, dom_overlay_container, fnStartAnimation;
+var canvas, canvas2, canvas3, stage, exportRoot, anim_container, dom_overlay_container, fnStartAnimation;
 function initPlanets() {
     canvas = document.getElementById("moonCanvas");
     canvas2 = document.getElementById('marsCanvas');
+    canvas3 = document.getElementById('neptuneCanvas');
     // moon
     var comp = AdobeAn.getComposition("A3AB6E8092563F44A2D2FE775D5778E1");
     // mars
     var comp2 = AdobeAn.getComposition("D7E3423B551E00458CD72DB4C211F4E1");
+    // neptune
+    var comp3 = AdobeAn.getComposition('D3ABBD737DF5094980460F0B3F29EB51');
     // moon
     var lib = comp.getLibrary();
     // mars
     var lib2 = comp2.getLibrary();
+    // neptune
+    var lib3 = comp3.getLibrary();
     //moon
     var loader = new createjs.LoadQueue(false);
     // mars
     var loader2 = new createjs.LoadQueue(false);
+    // neptune
+    var loader3 = new createjs.LoadQueue(false);
     // moon
     loader.addEventListener("fileload", function (evt) { handleMoonFileLoad(evt, comp) });
     loader.addEventListener("complete", function (evt) { handleMoonComplete(evt, comp) });
     // mars
     loader2.addEventListener("fileload", function (evt) { handleMarsFileLoad(evt, comp2) });
     loader2.addEventListener("complete", function (evt) { handleMarsComplete(evt, comp2) });
+    // neptune
+    loader3.addEventListener("fileload", function (evt) { handleNeptuneFileLoad(evt, comp3) });
+    loader3.addEventListener("complete", function (evt) { handleNeptuneComplete(evt, comp3) });
     // moon
     var lib = comp.getLibrary();
     // mars
     var lib2 = comp2.getLibrary();
+    // neptune
+    var lib3 = comp3.getLibrary();
     // moon
     loader.loadManifest(lib.properties.manifest);
     // mars
     loader2.loadManifest(lib2.properties.manifest);
+    // neptune
+    loader3.loadManifest(lib3.properties.manifest);
 }
+
+// ! MOON
 function handleMoonFileLoad(evt, comp) {
     var images = comp.getImages();
     if (evt && (evt.item.type == "image")) { images[evt.item.id] = evt.result; }
@@ -56,6 +72,7 @@ function handleMoonComplete(evt, comp) {
     AdobeAn.compositionLoaded(lib.properties.id);
     fnStartAnimation();
 }
+// ! MARS
 function handleMarsFileLoad(evt, comp2) {
     var images = comp2.getImages();
     if (evt && (evt.item.type == "image")) { images[evt.item.id] = evt.result; }
@@ -79,6 +96,34 @@ function handleMarsComplete(evt, comp2) {
     }
     //Code to support hidpi screens and responsive scaling.
     AdobeAn.makeResponsive(false, 'both', false, 1, [canvas2, anim_container, dom_overlay_container]);
+    AdobeAn.compositionLoaded(lib.properties.id);
+    fnStartAnimation();
+}
+// ! NEPTUNE
+function handleNeptuneFileLoad(evt, comp3) {
+    var images = comp3.getImages();
+    if (evt && (evt.item.type == "image")) { images[evt.item.id] = evt.result; }
+}
+
+function handleNeptuneComplete(evt, comp3) {
+    //This function is always called, irrespective of the content. You can use the variable "stage" after it is created in token create_stage.
+    var lib = comp3.getLibrary();
+    var ss = comp3.getSpriteSheet();
+    var queue = evt.target;
+    var ssMetadata = lib.ssMetadata;
+    for (i = 0; i < ssMetadata.length; i++) {
+        ss[ssMetadata[i].name] = new createjs.SpriteSheet({ "images": [queue.getResult(ssMetadata[i].name)], "frames": ssMetadata[i].frames })
+    }
+    exportRoot = new lib.savedAnNeptune_HTML5Canvas();
+    stage = new lib.Stage(canvas3);
+    //Registers the "tick" event listener.
+    fnStartAnimation = function () {
+        stage.addChild(exportRoot);
+        createjs.Ticker.framerate = lib.properties.fps;
+        createjs.Ticker.addEventListener("tick", stage);
+    }
+    //Code to support hidpi screens and responsive scaling.
+    AdobeAn.makeResponsive(false, 'both', false, 1, [canvas3, anim_container, dom_overlay_container]);
     AdobeAn.compositionLoaded(lib.properties.id);
     fnStartAnimation();
 }
