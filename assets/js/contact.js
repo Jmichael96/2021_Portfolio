bc18c19ea837a27021f027042a6981e4b735ec07
Jeffrey VanHorn <jeffreyvh@ymail.com>
2021-01-28 17:48:25 -0600

    eliminated cdn for adobe animations

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -66,15 +66,15 @@ function initContactContent() {
 };
 
 
-let map = tt.map({
-    key: 'idwJh8J0x9cq7vGdT0SgaIkuv0Gdb1pM',
-    container: 'map',
-    basePath: '/',
-    style: '/custommap.json',
-    source: 'vector',
-    center: [-97, 29],
-    zoom: 4
-});
+// let map = tt.map({
+//     key: 'idwJh8J0x9cq7vGdT0SgaIkuv0Gdb1pM',
+//     container: 'map',
+//     basePath: '/',
+//     style: '/custommap.json',
+//     source: 'vector',
+//     center: [-97, 29],
+//     zoom: 4
+// });
 
 $(window).on('load', function () {
     let hasReached = false;
@@ -99,19 +99,19 @@ $(window).on('load', function () {
     $('.locationEmail').attr('href', `mailto:${email1}${email2}${email3}`);
 });
 
-function createMarker(position, color, popupText) {
-    let markerElement = document.createElement('div');
-    markerElement.className = 'marker';
+// function createMarker(position, color, popupText) {
+//     let markerElement = document.createElement('div');
+//     markerElement.className = 'marker';
 
-    let markerContentElement = document.createElement('div');
-    markerContentElement.className = 'marker-content';
-    markerContentElement.style.backgroundColor = color;
-    markerElement.appendChild(markerContentElement);
+//     let markerContentElement = document.createElement('div');
+//     markerContentElement.className = 'marker-content';
+//     markerContentElement.style.backgroundColor = color;
+//     markerElement.appendChild(markerContentElement);
 
-    // add marker to map
-    new tt.Marker({ scale: 1.3 })
-        .setLngLat(position)
-        .addTo(map);
-};
+//     // add marker to map
+//     new tt.Marker({ scale: 1.3 })
+//         .setLngLat(position)
+//         .addTo(map);
+// };
 
-createMarker([-95.461195, 30.168032], '#3b4e74', 'The Woodlands, TX');
+// createMarker([-95.461195, 30.168032], '#3b4e74', 'The Woodlands, TX');
