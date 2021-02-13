b08726c209e913da2c46dba83b0a419adea4a0d1
Jeffrey VanHorn <jeffreyvh@ymail.com>
2021-02-12 18:02:46 -0600

    created a new alert box for contact form

---
+++ php/mail.php
@@ -0,0 +1,28 @@
+<?php
+
+if($_POST['action'] == 'callForm') {
+                $mail_to_send_to = "jeffreyvh@ymail.com" ;
+                $from_email = "admin@codevh.com" ;
+                $name = $_POST['name'] ;
+                $email = $_POST['email'] ;
+                $message = $_POST['message'] ;
+                // headers
+                $headers = "From: $from_email" . "\r\n" . "Reply-To: $mail_to_send_to" . "\r\n" ;
+                $headers = "MIME-Version: 1.0" . "\r\n" ;
+                $headers = "Content-type: text/html; multipart/mixed; charset=iso-8859-1" .  "\r\n" ;
+                $body =
+                "<html>
+                <head>
+                <title>Hello Dahmer</title>
+                </head>
+                <body>
+                <p>Message: $message</p>
+                <br />
+                <p>Name: $name</p>
+                <p>Email: <a href='#!'>$email</a></p>
+                </body>
+                 </html>
+                " ;
+                mail( $mail_to_send_to, "Message from your website", $body, $headers );
+        }
+?>
